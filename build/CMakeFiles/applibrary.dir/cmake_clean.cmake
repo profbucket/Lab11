4913236c57277c90e2f954abb089c1ad6d65a737
nicodeal <nicodeal@mail.uc.edu>
2023-12-04 00:10:34 -0500

    putting code into repo, awesome

---
+++ build/CMakeFiles/applibrary.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/applibrary.dir/whattotest.cpp.o"
+  "CMakeFiles/applibrary.dir/whattotest.cpp.o.d"
+  "libapplibrary.pdb"
+  "libapplibrary.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/applibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
